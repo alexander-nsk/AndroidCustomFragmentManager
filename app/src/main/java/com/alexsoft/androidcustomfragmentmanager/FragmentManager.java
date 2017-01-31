eb3f9863d33080fb9a43f7bad497569279e4dc56
Alex <dudko.nsk@gmail.com>
2017-01-31 17:35:54 +0700

    Re-factored the pop() method. Added the exception throw.

--- app/src/main/java/com/alexsoft/androidcustomfragmentmanager/FragmentManager.java
+++ app/src/main/java/com/alexsoft/androidcustomfragmentmanager/FragmentManager.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by Alex on 30.01.2017.
@@ -13,7 +12,7 @@ import java.util.List;
 
 public class FragmentManager {
 
-    private List<Fragment> fragmentList = new LinkedList<>();
+    private LinkedList<Fragment> fragmentList = new LinkedList<>();
     private LinearLayout rootView;
 
     public FragmentManager(Context context) {
@@ -35,20 +34,19 @@ public class FragmentManager {
     /**
      * Remove the last fragment from list
      */
-    public void pop() {
-        int size = fragmentList.size();
-        if (size == 0) {
+    public void pop() throws Exception {
+        if (fragmentList.isEmpty()) {
+            throw new Exception("Can't remove the last fragment from list. Cause: List is empty");
+        }
+
+        fragmentList.removeLast();                  //remove last fragment from list
+
+        if (fragmentList.isEmpty()) {               //check if list is empty
             return;
         }
 
-        fragmentList.remove(size - 1);
-
-        size = fragmentList.size();
-        if (size == 0) {
-            return;
-        }
-
-        rootView.addView(fragmentList.get(size - 1));
+        rootView.removeAllViews();                  //remove all fragments from root view
+        rootView.addView(fragmentList.getLast());   //add the last fragment to root view
     }
 
     /**
